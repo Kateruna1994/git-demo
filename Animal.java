3fc8bf68324a4eb60781965d10a4dee480787e98
Kateruna1994 <ketringoncharuk1994@gmail.com>
2022-02-23 22:30:18 +0200

    added new code

--- Animal.java
+++ Animal.java
@@ -1,7 +1,12 @@
 public class Animal {
-    String name = "Rex"
+    String name = "Rex";
 	int age =5;
     public void bark(){
         System.out.println("Gav gav");
+    }
+    public int sumOfTwoNumber(){
+        int a= 4;
+        int b =8;
+     return a + b;
     }
 }
